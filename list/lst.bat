db135e08e82232b174c5e536ba535af4036488c4
unknown <Matthew@RIKARDDESK.(none)>
2013-10-14 22:00:15 -0400

    Cleaning up repositories as they were committed together in error

--- list/lst.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-c:\SourceCode\list\list\bin\Debug\list.exe
-FOR /f "tokens=2,*" %%a IN ('reg query HKCU\Environment /v tempListDir') DO SET tempListDir=%%b
-
-echo here
-cd %tempListDir%
-cls
-@echo on
