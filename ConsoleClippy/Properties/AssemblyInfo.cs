638607663e04fe53915f6951d0d3c66635e6dc2b
Matthew Rikard <mnrikard@gmail.com>
2015-01-08 23:00:03 -0500

    cleaning

--- ConsoleClippy/Properties/AssemblyInfo.cs
+++ ConsoleClippy/Properties/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.*")]
-[assembly: AssemblyFileVersion("3.0.1.11")]
+[assembly: AssemblyFileVersion("3.0.1.13")]
